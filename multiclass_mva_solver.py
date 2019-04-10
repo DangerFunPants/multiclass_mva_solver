5dbf46ca5430d5b7447db2bab1ec8ab10eea678e
DangerFunPants <alexander.james@ucalgary.ca>
2019-04-09 18:57:24 -0600

    Cleanup.

--- multiclass_mva_solver.py
+++ multiclass_mva_solver.py
@@ -1,7 +1,6 @@
 
 from collections import defaultdict
 import pprint as pp
-import copy
 
 class Model_Spec:
     def __init__(self):
@@ -19,12 +18,6 @@ class Model_Spec:
         # Treat think independant of delay resources.
         # think :: c -> \mathbb{R}
         self._think = {}
-
-    # deprecated
-    def add_server(self, name):
-        for c_ident in self._demands.keys():
-            # initialize demands to zero for new resources.
-            self._demands[c_ident][name] = 0.0
 
     def add_queueing_center(self, name):
         self._queueing_centers.add(name)
@@ -63,19 +56,20 @@ class Model_Spec:
         if key == 0:
             return len(self._ns)
         elif key == 1:
-            return copy.deepcopy(self._demands)
+            return self.demands
         elif key == 2:
             return len(self._queueing_centers)
         elif key == 3:
-            return copy.deepcopy(self._think)
+            return self._think
         elif key == 4:
-            return copy.deepcopy(self._ns)
+            return self._ns
         elif key == 5:
             return self._queueing_centers
         elif key == 6:
             return self._delay_centers
         else:
-            raise IndexError("Attempted to unpack Model_Spec into more than four params")
+            raise IndexError("""Attempted to unpack Model_Spec 
+                    into more than four params""")
 
     def get_demands(self):
         return self._demands
@@ -104,16 +98,9 @@ class Model_Solution:
     def throughput(self, class_name):
         return self._throughputs[class_name]
 
-    def response_time(self, class_name):
-        return sum(self._response_times[class_name])
-
-    def detailed_response_time(self, class_name):
-        return self._response_times[class_name]
-
-    def get_response_times(self):
+    @property
+    def response_times(self):
         return self._response_times
-
-    response_times = property(get_response_times)
 
     def queue_lengths(self, class_name):
         return self._queue_lengths[class_name]
@@ -129,11 +116,13 @@ class Model_Solution:
         return (r / u) - r
 
     def idle_times(self):
-        return {c_name: self.idle_time(c_name) for c_name in self.devices()}
+        return {c_name: self.idle_time(c_name) for c_name in self.devices}
     
+    @property
     def classes(self):
         return next(iter(self._response_times.values())).keys()
 
+    @property
     def devices(self):
         return next(iter(self._response_times.values())).keys()
 
@@ -183,7 +172,7 @@ def multiclass_mva_solver(model):
             if ns[c] == 0:
                 R[c] = {k: 0 for k in device_names}
             else:
-                new_vector = copy.deepcopy(ns)
+                new_vector = dict(ns)
                 new_vector[c] -= 1
                 next_iter_model = build_model(delay_centers, queueing_centers,
                         D, Z, new_vector)
@@ -196,7 +185,9 @@ def multiclass_mva_solver(model):
             if ns[c] == 0:
                 X[c] = 0
             else:
-                X[c] = ns[c] / (Z[c] + model.get_infinite_server_time(c) + sum(R[c].values()))
+                numer = Z[c] + model.get_infinite_server_time(c)
+                numer += sum(R[c].values())
+                X[c] = ns[c] / numer
         
         Q = {}
         for k in device_names:
@@ -206,22 +197,7 @@ def multiclass_mva_solver(model):
 
 def solve_mva_model(model):
     Q, X, R = multiclass_mva_solver(model)
-    
     return Model_Solution(X, R, Q, model._think)
-
-def throughput(model_output):
-    Q, X, R = model_output
-    return X
-
-def response_time(model_output):
-    Q, X, R = model_output
-    # R :: class -> resource -> response time
-    return {c_i: sum(response_times.values()) 
-                for c_i, response_times in R.items()}
-
-def detailed_response_time(model_output):
-    Q, X, R = model_output
-    return R
 
 def compute_abs_error(expected, actual):
     numer = abs(expected - actual)
@@ -240,18 +216,17 @@ def check_error_tolerance(hw_results, sw_models):
 
 # For now this method assumes that the sw models appear in order in the list. 
 # This is easy to fix, construct a DAG where edges are clients consuming resources at a
-# queueing center then perform a topological sort of the DAG. This will partition the DAG
-# into layers, each of which corresponds to a single S/W model.
+# queueing center then perform a topological sort of the DAG. This will partition 
+# the DAG into layers, each of which corresponds to a single S/W model.
 def solve_lqm_sw_models(sw_models):
-    results = {}
+    results = []
     for idx in range(len(sw_models)-1):
         model_to_solve = sw_models[idx]
         model_result = solve_mva_model(model_to_solve)
+        results.append(model_result)
         # Compute idle times for all s/w resources in level idx + 1
         idle_times = model_result.idle_times()
         for class_name, idle_time in idle_times.items():
-            results[class_name] = idle_time
-            # @Fixme: Add to appropriate delay resource, not think time.
             sw_models[idx+1].add_think_time(class_name, idle_time)
         
     return results
@@ -260,12 +235,16 @@ def solve_lqm_model(sw_models, hw_model, tolerance):
     """
     sw_models   :: [Model_Spec]
     hw_model    :: Model_Spec
-    Note that sw_models contains all (L) software models even though only L-1 models
+    Note that sw_models contains all L software models even though only L-1 models
     are actually solved
     """
-
     while True:
-        sw_proc_idle_times = solve_lqm_sw_models(sw_models)
+        sw_model_results = solve_lqm_sw_models(sw_models)
+        sw_proc_idle_times = {}
+        for model_result in sw_model_results:
+            for class_name, idle_time in model_result.idle_times().items():
+                sw_proc_idle_times[class_name] = idle_time
+
         for class_name, idle_time in sw_proc_idle_times.items():
             hw_model.add_think_time(class_name, idle_time)
         hw_model_results = solve_mva_model(hw_model)
@@ -273,7 +252,17 @@ def solve_lqm_model(sw_models, hw_model, tolerance):
         if max(abs_errors) < tolerance:
             break
 
-    return hw_model_results
+        # Update demand of delay (H/W) resources in S/W modles
+        hw_response_times = hw_model_results.response_times
+        for sw_model in sw_models:
+            for class_name, device_dict in sw_model.demands.items():
+                for device_name, demand in device_dict.items():
+                    if device_name in sw_model.delay_centers:
+                        projected_demand = hw_response_times[class_name][device_name]
+                        sw_model[class_name][device_name] = projected_demand
+
+
+    return (sw_model_results, hw_model_results)
 
 def main():
     def sw_model2():
@@ -304,11 +293,6 @@ def main():
         return (delay_centers, queueing_centers, D, Z, ns)
 
     def hw_model():
-        # Not sure what the number of customers should be?
-        # Since the question states that everything executes synchronously 
-        # I would assume one but that could most certianly be incorrect. 
-        # 0 is web, 1 is db
-        # ns = [1, 1] 
         ns = { "web_server": 1
              , "db_server": 1
              }
@@ -327,8 +311,9 @@ def main():
                 , build_model(*sw_model0())
                 ]
     hw_model = build_model(*hw_model())
-    model_results = solve_lqm_model(sw_models, hw_model, 0.1)
-    print(model_results)
+    
+    sw_model_results, hw_model_results = solve_lqm_model(sw_models, hw_model, 0.1)
+    pp.pprint(sw_model_results[0])
 
 if __name__ == "__main__":
     main()
